7b85f8f7d13405ca51cbce2adf599fc7d500311c
Joshua <131817672+2023JoshuaP@users.noreply.github.com>
2024-08-02 14:13:42 -0500

    Update BufferPool.h

--- Buffer/BufferPool.h
+++ Buffer/BufferPool.h
@@ -15,7 +15,7 @@ class BufferPool {
         ~BufferPool();
         void agregarNuevaPaginaBufferPool(int numFrame, int numPagina);
         void mostrarFramePagina(int numPagina);
-        void lecturaOescrituraPagina(int numPagina);
+        void lecturaOescrituraPagina(int numPagina, BPlusTree* Btree);
         void agregarContenidoPaginaAbloque(int numPagina,int numPaginaEliminada);
         void verificarExistenciaRegistro(string &registroPagina);
         void buscarRegistro(string& registroFrame, int frameElegido);
